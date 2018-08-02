081a7cfe1ba5372e1c194df846365c1d9f10cec8
Joseph Smith <joe@twocards.co>
2018-08-02 04:04:36 +0100

    End of lesson 2

--- app/server.js
+++ app/server.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const dotenv = require('dotenv');
 
 const app = express(); // This is used to create the express app
 
-app.listen(2000, (err) => {
+// Setup the app enviroment variables
+dotenv.config({
+  path: './.env'
+});
+
+app.listen(process.env.PORT, (err) => {
   if (err) {
     console.log(err); // TODO: Improve error handling
     process.exit(1);
   }
 
-  console.log('Your server is now running');
+  console.log(`Server is now running on port ${process.env.PORT} in ${process.env.NODE_ENV} mode.`);
 });

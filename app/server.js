dcb8289c8b6b2756877e726a2d4903b2ebf4768a
Joseph Smith <joe@twocards.co>
2018-08-12 18:01:11 +0100

    End of lesson 8

--- app/server.js
+++ app/server.js
@@ -2,8 +2,12 @@ const express = require('express');
 const dotenv = require('dotenv');
 const fs = require('fs');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express(); // return instance of the app
+
+// Setting up the cors config
+app.use(cors());
 
 // Tell the app to parse the body of the request
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -27,5 +31,5 @@ app.listen(process.env.PORT, (err) => {
 
   require('./utils/db');
 
-  console.log(`Server is now running in ${process.env.NODE_ENV} on port ${process.env.PORT}`);
+  console.log(`CORS-enabled web server is now running in ${process.env.NODE_ENV} on port ${process.env.PORT}`);
 });
